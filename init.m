b41118b5ef12b830beaf056c67cd37420887bf29
hohilwik <hohilwik@MSI.localdomain>
2021-08-06 22:09:49 +0530

    added init and required sample

---
+++ init.m
@@ -0,0 +1,5 @@
+ISM_RIR_bank(ISM_setup,'ISM_RIRs.mat');
+dualchannelaudio = audioread('sample_classical_music.wav');
+SrcSignalVec = y(:,1);
+AuData = ISM_AudioData('ISM_RIRs.mat',SrcSignalVec);
+audiowrite('sample_with_echo.wav', AuData, 44100);
